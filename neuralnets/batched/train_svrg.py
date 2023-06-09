bc6f903e3cea5a7442b2147d1bb1154bedf089de
Mohamad Fakhouri <mohahf19@gmail.com>
2023-06-09 12:17:47 +0200

    added argument parser for batched config.py

--- neuralnets/batched/train_svrg.py
+++ neuralnets/batched/train_svrg.py
@@ -1,22 +1,22 @@
 import pickle
 from collections.abc import Iterator
+from copy import deepcopy
 from pathlib import Path
+
 import numpy as np
 import torch
-from copy import deepcopy
-
 from config import (
-        device_c,
-        network_c,
-        network_temp_c,
-        num_epochs_c,
-        output_dir_c,
-        batch_size_c,
-        test_loader_c,
-        train_loader_c,
-        train_loader_temp_c,
-        test_every_x_steps_c
-    )
+    batch_size_c,
+    device_c,
+    network_c,
+    network_temp_c,
+    num_epochs_c,
+    output_dir_c,
+    test_every_x_steps_c,
+    test_loader_c,
+    train_loader_c,
+    train_loader_temp_c,
+)
 from svrg import SVRG
 from tqdm import tqdm
 from utils import visualize_losses
@@ -39,30 +39,28 @@ for run in range(5):
 
     criterion = torch.nn.CrossEntropyLoss()
 
-
-    steps_per_epoch = len(train_loader.dataset)//batch_size
-    decrease_lr_epochs = [150,220]
+    steps_per_epoch = len(train_loader.dataset) // batch_size
+    decrease_lr_epochs = [150, 220]
     optimizer = SVRG(
         network.parameters(),
         lr=0.1,
-        weight_decay = 0.0001,
-        snapshot_rand = True,
-        prob_snapshot=4 / steps_per_epoch, # twice per epoch in expectation
-        steps_per_snapshot = steps_per_epoch,
+        weight_decay=0.0001,
+        snapshot_rand=True,
+        prob_snapshot=4 / steps_per_epoch,  # twice per epoch in expectation
+        steps_per_snapshot=steps_per_epoch,
         nn=network_temp,
         loss_func=criterion,
         data_loader=train_loader_temp,
         device=device,
         lr_decrease=0.1,
-        decrease_step=[(epoch-1)*steps_per_epoch for epoch in decrease_lr_epochs],
-        momentum=0.9
+        decrease_step=[(epoch - 1) * steps_per_epoch for epoch in decrease_lr_epochs],
+        momentum=0.9,
     )
 
     def tensor_to_arr_or_scalar(tensor: torch.Tensor):
         if tensor.numel() == 1:
             return tensor.item()
         return tensor.detach().cpu().numpy()
-
 
     def train_step(tr_loader_iterator: Iterator, device: str = "cpu"):
         network.train()
@@ -72,8 +70,24 @@ for run in range(5):
         optimizer.zero_grad()
         loss = criterion(output, target)
         loss.backward()
-        took_snapshot, variance_term, grad_term, snap_dist, dist, sgd_step = optimizer.step(x=data, y=target)
-        return loss.item(), took_snapshot, indices, variance_term, grad_term, snap_dist, dist, sgd_step
+        (
+            took_snapshot,
+            variance_term,
+            grad_term,
+            snap_dist,
+            dist,
+            sgd_step,
+        ) = optimizer.step(x=data, y=target)
+        return (
+            loss.item(),
+            took_snapshot,
+            indices,
+            variance_term,
+            grad_term,
+            snap_dist,
+            dist,
+            sgd_step,
+        )
 
     def test(device: str = "cpu"):
         network.train(False)
@@ -84,7 +98,6 @@ for run in range(5):
             loss = criterion(output, target)
             running_loss += loss.item()
         return running_loss / len(test_loader)
-
 
     def train(weights_folder: Path, num_epochs: int = 1, device: str = "cpu"):
         tr_losses = []
@@ -98,9 +111,9 @@ for run in range(5):
         avg_grad = []
         count = 0.0
 
-        #parameter for the moving average
+        # parameter for the moving average
         alpha = 0.25
-    
+
         tr_loader_iterator = iter(train_loader)
         # Save initial weights
         torch.save(network.state_dict(), weights_folder / "initial_weights.pt")
@@ -114,50 +127,69 @@ for run in range(5):
                 loss = criterion(output, target)
                 loss.backward()
 
-                took_snapshot, variance_term, grad_term, snap_dist, dist, sgd_step = optimizer.step(step=step,x=data, y=target)
+                (
+                    took_snapshot,
+                    variance_term,
+                    grad_term,
+                    snap_dist,
+                    dist,
+                    sgd_step,
+                ) = optimizer.step(step=step, x=data, y=target)
 
                 indices.append((step, tensor_to_arr_or_scalar(index)))
                 tr_losses.append((step, loss.item()))
                 took_snapshots.append((step, took_snapshot))
-                #computing the moving variance and mean of the gradient estimate
+                # computing the moving variance and mean of the gradient estimate
                 if len(avg_grad) == 0:
                     avg_grad = grad_term
                 else:
                     for j, g in enumerate(grad_term):
                         avg_grad[j] += g
-                count +=1
+                count += 1
                 var_term = 0.0
                 for var, avg_g in zip(variance_term, avg_grad):
-                    var_term += torch.norm(var - avg_g/count)**2
+                    var_term += torch.norm(var - avg_g / count) ** 2
                 var_term_sgd = 0.0
                 for sgd, avg_g in zip(sgd_step, avg_grad):
-                    var_term_sgd += torch.norm(sgd - avg_g/count)**2
+                    var_term_sgd += torch.norm(sgd - avg_g / count) ** 2
 
                 if len(moving_variance) == 0:
                     moving_variance.append(var_term)
                 else:
-                    new_variance = (1 - alpha)*moving_variance[-1] + alpha*var_term
+                    new_variance = (1 - alpha) * moving_variance[-1] + alpha * var_term
                     moving_variance.append(new_variance)
 
                 if len(moving_variance_sgd) == 0:
                     moving_variance_sgd.append(var_term_sgd)
                 else:
-                    new_variance = (1 - alpha)*moving_variance_sgd[-1] + alpha*var_term_sgd
+                    new_variance = (1 - alpha) * moving_variance_sgd[
+                        -1
+                    ] + alpha * var_term_sgd
                     moving_variance_sgd.append(new_variance)
-                print(moving_variance[-1], moving_variance_sgd[-1])
-
+                # print(moving_variance[-1], moving_variance_sgd[-1])
 
                 snap_distances.append(snap_dist)
                 distances.append(dist)
                 if step % test_every_x_steps == 0:
-                    torch.save(network.state_dict(), weights_folder / f"weights_{step}.pt")
+                    torch.save(
+                        network.state_dict(), weights_folder / f"weights_{step}.pt"
+                    )
                     val_loss = test(device)
                     val_losses.append((step, val_loss))
-                    print(f"Step: {step}, Train Loss: {loss.item()}, Test Loss: {val_loss}")
+                    print(
+                        f"Step: {step}, Train Loss: {loss.item()}, Test Loss: {val_loss}"
+                    )
                 step += 1
-        
-        return tr_losses, val_losses, took_snapshots, indices, moving_variance, snap_distances, distances
 
+        return (
+            tr_losses,
+            val_losses,
+            took_snapshots,
+            indices,
+            moving_variance,
+            snap_distances,
+            distances,
+        )
 
     output_dir = output_dir / f"svrg_{run}"
     output_dir.mkdir(exist_ok=True, parents=True)
@@ -166,9 +198,15 @@ for run in range(5):
     weights_dir.mkdir(exist_ok=True, parents=True)
 
     print("Started training..")
-    tr_losses, val_losses, took_snapshots, indices, moving_variance, snap_distances, distances = train(
-        num_epochs=num_epochs, weights_folder=weights_dir, device=device
-    )
+    (
+        tr_losses,
+        val_losses,
+        took_snapshots,
+        indices,
+        moving_variance,
+        snap_distances,
+        distances,
+    ) = train(num_epochs=num_epochs, weights_folder=weights_dir, device=device)
 
     with open(output_dir / f"results_{run}.pkl", "wb") as f:
         pickle.dump(
@@ -179,9 +217,7 @@ for run in range(5):
                 "sampled_indices": indices,
                 "variances": moving_variance,
                 "snap_distances": snap_distances,
-                "distances": distances
+                "distances": distances,
             },
             f,
         )
-
-
