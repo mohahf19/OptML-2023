edf0587d01a12096ff27f1dbd844e4124c840972
Davide Mazzali <davidemazzali98@gmail.com>
2023-06-14 16:29:14 +0200

    new experiments

--- output/convert.py
+++ output/convert.py
@@ -5,10 +5,10 @@ from pathlib import Path
 to_float = lambda x: x.item() if torch.is_tensor(x) and x.shape == torch.Size([]) else x
 unpack = lambda x: (x[0][0], to_float(x[0][1])) if type(x[0]) == tuple and len(x[0]) == 2 else (x[1], to_float(x[0]))
 
-num_runs = 4
+num_runs = 5
 experiment = "saga_p1_runs_bs1_lr1e-02"
 
-for run in range(num_runs):
+for run in range(4, 5):
     pickled_data = open(f"{experiment}/{run}/train_data.pkl", "rb")
     results = pickle.load(pickled_data)
 
